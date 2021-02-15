262ba0121742b9f320d4b913c1b0e0c3f71561bf
ivan <ivan@localhost.localdomain>
2021-02-15 21:24:50 +0100

    Correcio 09-tipus-file.sh

--- 09-tipus-file.sh
+++ 09-tipus-file.sh
@@ -2,8 +2,8 @@
 # !/bin/bash
 # @edt ASIX M01-ISO
 # Febrer 2021
-# llistar el direcotri si existeix + manual d'ajuda -h
-#  $ prog arg (dir o -h)
+# Dir si el argument introduit es un direcotri, un link o un regular file
+#  $ prog arg (direcotri,link o regular file)
 # -------------------------------------
 ERR_NARGS=1
 ERR_NOFILE=2
@@ -12,24 +12,24 @@ ERR_NOFILE=2
 if [ $# -ne 1 ]
 then
     echo "Error: nºarguments incorrecte"
-    echo "Usage: $0 directori"
+    echo "Usage: $0 arg (direcotri,link o regular file)"
     exit $ERR_NARGS
 fi
 
 directori=$1
 
-if [ -d $directori ]
+if [ -d $directori ] # Validem si es un directori
 then
     echo "Es un directori"
-elif [ -L $directori ]
+elif [ -L $directori ] # Validem si es un link
 then
     echo "Es un link"
-elif [ -f $directori ]
+elif [ -f $directori ] # Validem si es un regular file
 then
     echo "Es un regular file"
-else
+else  # Si no es cap dels anteriors
     echo "Error: No s'ha trobat el fitxer especificat"
-    echo "Usage: $0 'direcotri,link o regular file'"
+    echo "Usage: $0 arg (direcotri,link o regular file)"
     exit $ERR_NOFILE
 fi
 
